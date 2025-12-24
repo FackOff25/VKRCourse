4f3b632d588fa274ea2229028b418a13bb4ee0bc
fackoff <fackoff@DESKTOP-OUU7392.lan>
2025-12-25 00:02:23 +0300

    graph

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,9 +1,10 @@
 #ifndef VKR_COURSE_GRAPH
 #define VKR_COURSE_GRAPH
 
-#include<stdlib.h>
+#include <stdlib.h>
 #include <map>
 #include <string>
+#include <vector>
 
 class INodeKey{
     virtual bool operator<(const INodeKey& other);
@@ -20,11 +21,22 @@ class Parameter{
     virtual data_type  get_data();
 };
 
+template <typename NodeKey>
 class INode {
 public:
-    const INodeKey key;
+    INode(NodeKey _key): key(_key);
+    INode(NodeKey _key, std::vector<NodeKey> _this_storage_neighbours): key(_key), this_storage_neighbours(_this_storage_neighbours);
+    INode(NodeKey _key, std::vector<NodeKey> _this_storage_neighbours, std::map<int,std::vector<NodeKey>> _other_storages_neighbours): key(_key), this_storage_neighbours(_this_storage_neighbours), other_storages_neighbours(_other_storages_neighbours);
+
+    static_assert(std::is_base_of<INodeKey, NodeKey>::value, "NodeKey must be a type derived from INodeKey");
+    
+    const NodeKey key;
 
     std::map<std::string, Parameter<char[]>> parameters;
+
+    std::vector<NodeKey> this_storage_neighbours;
+
+    std::map<int,std::vector<NodeKey>> other_storages_neighbours;
 };
 
 #endif
