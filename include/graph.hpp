667ff16897794bd93a91119cff566296323517ac
fackoff <fackoff@DESKTOP-OUU7392.lan>
2025-12-25 00:47:09 +0300

    graph

--- include/graph.hpp
+++ include/graph.hpp
@@ -5,6 +5,8 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <numeric>
+#include <utility>
 
 template <typename KeyType> 
 class NodeKey {
@@ -31,21 +33,41 @@ class Parameter{
         };
 };
 
+class Edge {
+    public:
+        const int weight;
+        std::map<std::string, Parameter<char*>> parameters;
+};
+
 template <typename KeyType>
 class Node {
     public:
+        typedef std::pair<NodeKey<KeyType>,Edge> Neighbour;
+
         Node(KeyType _key): key(NodeKey<KeyType>(_key)) {};
         Node(NodeKey<KeyType> _key): key(_key) {};
-        Node(NodeKey<KeyType> _key, std::vector<NodeKey<KeyType>> _this_storage_neighbours): key(_key), this_storage_neighbours(_this_storage_neighbours) {};
-        Node(NodeKey<KeyType> _key, std::vector<NodeKey<KeyType>> _this_storage_neighbours, std::map<int,std::vector<NodeKey<KeyType>>> _other_storages_neighbours): key(_key), this_storage_neighbours(_this_storage_neighbours), other_storages_neighbours(_other_storages_neighbours) {};
+        Node(NodeKey<KeyType> _key, std::vector<Neighbour> _this_storage_neighbours): key(_key), this_storage_neighbours(_this_storage_neighbours) {};
+        Node(NodeKey<KeyType> _key, std::vector<Neighbour> _this_storage_neighbours, std::map<int,std::vector<Neighbour>> _other_storages_neighbours): key(_key), this_storage_neighbours(_this_storage_neighbours), other_storages_neighbours(_other_storages_neighbours) {};
 
         const NodeKey<KeyType> key;
-
         std::map<std::string, Parameter<char*>> parameters;
 
-        std::vector<NodeKey<KeyType>> this_storage_neighbours;
+        std::vector<Neighbour> this_storage_neighbours;
+        std::map<int,std::vector<Neighbour>> other_storages_neighbours;
 
-        std::map<int,std::vector<NodeKey<KeyType>>> other_storages_neighbours;
+        void set_this_storage_neighbours(std::vector<Neighbour> neighbours) const {this_storage_neighbours = neighbours;};
+        void set_other_storages_neighbours(std::map<int,std::vector<Neighbour>> neighbours) const {other_storages_neighbours = neighbours;};
+
+        int get_edge_num_to_others() const {
+            return std::accumulate(
+                other_storages_neighbours.begin(),
+                other_storages_neighbours.end(),
+                0,
+                [](int sum, const auto& pair) {
+                    return sum + pair.second.size();
+                }
+            );
+        };
 };
 
 #endif
